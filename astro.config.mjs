9b0141c5790ca56d562298a7441d5fc6dbd00e88
Eliza Milburn <ERmilburn02@outlook.com>
2024-10-07 22:14:59 +0100

    Sessions

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,20 +9,35 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Lucia Next",
+      logo: {
+        src: "/src/assets/lucia-logo-square-transparent.webp",
+      },
       social: {
         github: "https://github.com/ERmilburn02/lucia-next-docs-starlight",
       },
       sidebar: [
+        //   {
+        //     label: "Guides",
+        //     items: [
+        //       // Each item here is one entry in the navigation menu.
+        //       { label: "Example Guide", slug: "guides/example" },
+        //     ],
+        //   },
+        //   {
+        //     label: "Reference",
+        //     autogenerate: { directory: "reference" },
+        //   },
         {
-          label: "Guides",
+          label: "Sessions",
           items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
+            { label: "Overview", slug: "sessions/overview" },
+            { label: "Basic API", slug: "sessions/basic-api" },
+            { label: "Cookies", slug: "sessions/cookies" },
+            {
+              label: "Migrate from Lucia v3",
+              slug: "sessions/migrate-lucia-v3",
+            },
           ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
         },
       ],
     }),
